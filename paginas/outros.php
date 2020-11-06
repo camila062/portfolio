fcd8e8be17070d0671b3eaf17ba10e30a4854ed0
camila062 <camilalidiaaraujo@gmail.com>
2020-11-06 15:39:00 -0300

    aloteração8

--- paginas/outros.php
+++ paginas/outros.php
@@ -4,34 +4,55 @@
     <h1 style="font-family: rose;">Outros</h1>
     <hr>
 
-    <div class="row">
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="public/img/fundo P1.jpg" alt="Imagem de capa do card">
-                <div class="card-body">
-                    <h5 class="lead">Em breve...</h5>
-                    <a href="#" target="blank" class="btn btn-primary">Visitar</a>
-                </div>
+    <div class="card-columns">
+        <div class="card">
+            <img class="" src="public/img/insatiable1.jpg" alt="Imagem de capa do card">
+            <div class="card-body">
+                <h5 class="card-title">Insatiable</h5>
+                <p class="lead">Insatiable é uma série de televisão norte-americana original Netflix de humor ácido, criada por Lauren Gussis. A série estreou em 10 de agosto de 2018 e é composta por 12 episódios. Em 12 de setembro de 2018, a série foi renovada para a segunda temporada, lançada em 11 de outubro de 2019.</p>
             </div>
         </div>
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="public/img/fundo P1.jpg" alt="Imagem de capa do card">
-                <div class="card-body">
-                    <h5 class="lead">Em breve...</h5>
-                    <a href="#" target="blank" class="btn btn-primary">Visitar</a>
-                </div>
+
+        <div class="card">
+            <div class="card-body">
+            <img class="" src="public/img/the1.jpg" alt="Imagem do card">
             </div>
         </div>
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="public/img/fundo P1.jpg" alt="Imagem de capa do card">
-                <div class="card-body">
-                    <h5 class="lead">Em breve...</h5>
-                    <a href="#" target="blank" class="btn btn-primary">Visitar</a>
-                </div>
+
+        <div class="card">
+            <img class="" src="public/img/atypical.jpg" alt="Imagem de capa do card">
+            <div class="card-body">
+                <h5 class="card-title">Atypical</h5>
+                <p class="lead">Atypical é uma série norte-americana de comédia dramática original Netflix, criada e escrita por Robia Rashid, que conta a história de um rapaz de 18 anos, diagnosticado dentro do espectro do autismo, que trabalha e estuda, vivendo a efervescência da idade e seu amadurecimento.</p>
             </div>
         </div>
+
+        <div class="card bg-primary text-white text-center p-3">
+            <blockquote class="blockquote mb-0">
+                <p style="font-family: rose;">Séries mais vistas e comentadas da Netflix</p>
+                <footer class="blockquote-footer">
+                <p class="card-text"><small class="text-muted">Atualizados 3 minutos atrás</small></p>
+                </footer>
+            </blockquote>
+        </div>
+
+        <div class="card">
+            <img class="" src="public/img/gif1.gif" alt="Imagem do card">
+        </div>
+        <div class="card p-3 text-right">
+            <blockquote class="blockquote mb-0">
+            <h5 class="card-title">Peaky Blinders</h5>
+                <p class="lead">Série sobre uma família de gângsteres na Inglaterra no começo do século 20.</p>
+            </blockquote>
+        </div>
+
+        <div class="card p-3">
+            <blockquote class="blockquote mb-0 card-body">
+                <h5 class="card-title">The End of the F***ing World</h5>
+                <p class="lead">The End of the F***ing World é uma série de televisão britânica absurdista dramática de humor ácido, baseada na série de quadrinhos The End of the Fucking World de Charles S. Forsman.</p>
+            </blockquote>
+        </div>
+
     </div>
 
 </div>
